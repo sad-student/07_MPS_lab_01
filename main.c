4f539199ba4de215190084cc337df734654f9478
Maxim Komlik <maxim.komlik@tut.by>
2021-09-15 18:50:17 +0300

    Button behavior corrections

--- main.c
+++ main.c
@@ -1,25 +1,46 @@
 #include <msp430.h> 
 
-// 7.1 2.2
+// L5:1.2 L4:1.1
+// S1:1.7 S2:2.2
+
+//#define S1_INV_BEH // enable if button S1 active level is high
+//#define S2_INV_BEH // enable if button S2 active level is high
 
 #pragma vector = PORT1_VECTOR
 __interrupt void S1_interrupt_handler(void)
 {
 	if(P1IFG & BIT7){
 		P1IFG &= ~BIT7;
-
+		P1IES ^= BIT7;
+#ifdef S2_INV_BEH
+	    if(P2IN & BIT2){
+#else
 	    if(~P2IN & BIT2){
-			P1IES ^= BIT7;
-
-			P1SEL &= ~BIT2;
-			P1DIR |= BIT2;
-			if(P1IN & BIT7){
-				// LED off
+#endif
+#ifdef S1_INV_BEH
+	    	if(~P1IES & BIT7){
+			//if(~P1IN & BIT7){
+#else
+	    	if(P1IES & BIT7){
+			//if(P1IN & BIT7){
+#endif
+				// LED5 off
 				P1OUT &= ~BIT2;
 
 			} else {
-				// LED on
+				// LED5 on
 				P1OUT |= BIT2;
+			}
+	    }else{
+#ifdef S1_INV_BEH
+	    	if(~P1IES & BIT7){
+			//if(~P1IN & BIT7){
+#else
+	    	if(P1IES & BIT7){
+	    	//if(P1IN & BIT7){
+#endif
+				// LED4 off
+				P1OUT &= ~BIT1;
 			}
 	    }
 	}
@@ -30,17 +51,8 @@ __interrupt void S2_interrupt_handler(void)
 {
 	if(P2IFG & BIT2){
 		P2IFG &= ~BIT2;
-
-	    if(~P2IN & BIT2){
-
-			P1SEL &= ~BIT1;
-			P1DIR |= BIT1;
-
-			if(P2IN & BIT2){
-				// LED on
-				P1OUT |= BIT1;
-			}
-	    }
+		// LED4 on
+		P1OUT |= BIT1;
 	}
 }
 
@@ -48,7 +60,7 @@ int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
     // LED initialization
-    P1SEL &= ~(BIT1 | BIT2 | BIT7);
+    P1SEL &= ~(BIT1 | BIT2);
     P1DIR |= (BIT1 | BIT2);
     P1OUT &= ~(BIT1 | BIT2);
 
@@ -56,8 +68,10 @@ int main(void) {
 
 
     // Button S1: pushed - 0, free - 1
+    P1SEL &= ~BIT7;
     P1DIR &= ~BIT7;
     P1REN |= BIT7;
+    // no need to switch depending on button behavior, actually works well on both scenarios
     P1IES = (P1IES & ~BIT7) | (P1IN & BIT7);
     P1IFG &= ~BIT7;
     P1IE |= BIT7;
@@ -66,8 +80,11 @@ int main(void) {
     P2SEL &= ~BIT2;
     P2DIR &= ~BIT2;
     P2REN |= BIT2;
+#ifdef S2_INV_BEH
     P2IES &= ~BIT2;
-    // P2IES = (P2IES & ~BIT2) | (P2IN & ~BIT2);
+#else
+    P2IES |= BIT2;
+#endif
     P2IFG &= ~BIT2;
     P2IE |= BIT2;
 
