7eb53e3a43353810dbc1a4cf2eefd3bcf7f6ba21
Maxim Komlik <maxim.komlik@tut.by>
2021-09-24 14:59:08 +0300

    Correct control looop implementation (base)

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 
 //#define S1_INV_BEH // enable if button S1 active level is high
 //#define S2_INV_BEH // enable if button S2 active level is high
+#define INT_CONTROL_IMPL // enable for interrupt implementation switching
 
 #pragma vector = PORT1_VECTOR
 __interrupt void S1_interrupt_handler(void)
@@ -56,6 +57,8 @@ __interrupt void S2_interrupt_handler(void)
 	}
 }
 
+void control_loop();
+
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
@@ -64,22 +67,27 @@ int main(void) {
     P1DIR |= (BIT1 | BIT2);
     P1OUT &= ~(BIT1 | BIT2);
 
+#ifdef INT_CONTROL_IMPL
     __bis_SR_register(GIE);
-
+#endif
 
     // Button S1: pushed - 0, free - 1
     P1SEL &= ~BIT7;
     P1DIR &= ~BIT7;
     P1REN |= BIT7;
+#ifdef INT_CONTROL_IMPL
     // no need to switch depending on button behavior, actually works well on both scenarios
     P1IES = (P1IES & ~BIT7) | (P1IN & BIT7);
     P1IFG &= ~BIT7;
     P1IE |= BIT7;
+#endif
 
     // Button S2: pushed - 1, free - 0
     P2SEL &= ~BIT2;
     P2DIR &= ~BIT2;
     P2REN |= BIT2;
+
+#ifdef INT_CONTROL_IMPL
 #ifdef S2_INV_BEH
     P2IES &= ~BIT2;
 #else
@@ -87,10 +95,44 @@ int main(void) {
 #endif
     P2IFG &= ~BIT2;
     P2IE |= BIT2;
+#endif
 
-
-    while(1){
-    	__no_operation();
-    }
+#ifndef INT_CONTROL_IMPL
+    control_loop();
+#endif
+    __no_operation();
 	return 0;
+}
+
+void control_loop(){
+	short int s_state = (P1IN & BIT7) | (P2IN & BIT2);
+	while(1){
+		if((s_state & BIT7) ^ (P1IN & BIT7)){
+			// button 1 event
+			if(~P2IN & BIT2){
+				if(P1IN & BIT7){
+					// LED5 off
+					P1OUT &= ~BIT2;
+
+				} else {
+					// LED5 on
+					P1OUT |= BIT2;
+				}
+			}else{
+				if(P1IN & BIT7){
+					// LED4 off
+					P1OUT &= ~BIT1;
+				}
+			}
+			s_state = (s_state & ~BIT7) | (P1IN & BIT7);
+		}
+		if((s_state & BIT2) ^ (P2IN & BIT2)){
+			// button 2 event
+			if(~P2IN & BIT2){
+				// LED4 on
+				P1OUT |= BIT1;
+			}
+			s_state = (s_state & ~BIT2) | (P2IN & BIT2);
+		}
+	}
 }
